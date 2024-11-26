8c0b56809122c533de7994cf5c1a49b96cda9aa7
lekehal <ilyes.lekehal-de-percin.etu@univ-artois.fr>
2024-11-26 10:55:26 +0100

    add crud

--- streemi/src/Controller/Other/CategoryController.php
+++ streemi/src/Controller/Other/CategoryController.php
@@ -4,14 +4,30 @@ declare(strict_types=1);
 
 namespace App\Controller\Other;
 
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class CategoryController extends AbstractController
 {
-    #[Route(path: '/categories', name: 'page_categories')]
-    public function categories()
+    #[Route(path: '/categories{id}', name: 'page_categories')]
+    public function categories(String $id,  EntityManagerInterface $entityManager,CategoryRepository $categoryRepository):Response
     {
-        return $this->render('other/discover.html.twig');
+
+        dump($id);
+        $categories=$categoryRepository ->findAll();
+        return $this->render('other/discover.html.twig',['categories'=>$categories]);
+    }
+
+    #[Route(path: '/categorie{id}', name: 'page_categories')]
+    public function getCategoriesId(String $id,  EntityManagerInterface $entityManager,Category $categorie):Response
+    {
+
+        dump($id);
+        return $this->render('movie/category.html.twig',['categorie'=>$categorie]);
     }
 }
